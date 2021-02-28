c50763c434f9e0b6f3d030a608ef580bad4724ee
alonbalbuena <alonbalbuena@gmail.com>
2021-02-28 20:01:39 +0100

    ✨ change web title

--- src/components/HeaderTitle.ts
+++ src/components/HeaderTitle.ts
@@ -5,7 +5,7 @@ class HeaderTitle extends Component {
     super();
     this.shadow.innerHTML = `
       <link rel="stylesheet" href="/public/styles/header-title.css">
-      <h1 class="title">Welcome to this site</h1>
+      <h1 class="title">S</h1>
       `;
   }
 }
