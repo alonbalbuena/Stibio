58bd27977aa9fbcc430b4283aa2b0752f86dc155
alonbalbuena <alonbalbuena@gmail.com>
2021-02-28 20:02:14 +0100

    💄 create backface for movie card

--- src/components/Movie.ts
+++ src/components/Movie.ts
@@ -1,16 +1,22 @@
 import Movie from "../models/movie.js";
 
 export default class MovieCard extends HTMLElement {
+  className = "movie";
+
   constructor(dataMovie: Movie) {
     super();
     this.attachShadow({ mode: "open" });
     if (this.shadowRoot) {
       this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="/public/styles/movie.css">
-      <h1 class="title">${dataMovie.title}</h1>
-      <h2>${dataMovie.release_date}</h2>
-      <h3>${dataMovie.overview}</h3>
-      <img src="${dataMovie.poster_path}" alt="${dataMovie.title}" loading="lazy"/>
+      <div class="front">
+        <img class="front__poster" src="${dataMovie.poster_path}" alt="${dataMovie.title}" loading="lazy"/>
+      </div>
+      <div class="back">
+        <h1 class="back__title">${dataMovie.title}</h1>
+        <h2 class="back__release">${dataMovie.release_date}</h2>
+        <p class="back__sinopsis">${dataMovie.overview}</p>
+      </div>
       `;
     }
   }
