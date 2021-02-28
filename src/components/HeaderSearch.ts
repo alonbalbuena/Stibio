9ddc9d01318c7aa2574c84467320b1867b04ece9
alonbalbuena <alonbalbuena@gmail.com>
2021-02-28 19:47:46 +0100

    ✨ create search component

---
+++ src/components/HeaderSearch.ts
@@ -0,0 +1,22 @@
+import Component from "./Component.js";
+
+export default class HeaderSearch extends Component {
+  constructor() {
+    super();
+    this.shadow.innerHTML = `
+      <link rel="stylesheet" href="/public/styles/header-search.css">
+      <glass-search></glass-search>
+      <form class="search-box" is="search-box" onsubmit="return false"></form>
+      `;
+  }
+
+  connectedCallback(): void {
+    this.addEventListener("click", () => this.open());
+  }
+
+  open(): void {
+    this.classList.toggle(`${this.classList[0]}--open`);
+  }
+}
+
+window.customElements.define("header-search", HeaderSearch);
