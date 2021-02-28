09df3398cfc003afcfbff49136f18fac81a4e738
alonbalbuena <alonbalbuena@gmail.com>
2021-03-01 00:56:42 +0100

    ♻️ move card syze to global CSS

--- src/components/Movie.ts
+++ src/components/Movie.ts
@@ -1,24 +1,31 @@
+import Component from "./Component.js";
 import Movie from "../models/movie.js";
+import Utils from "../utils/Utils.js";
 
-export default class MovieCard extends HTMLElement {
-  className = "movie";
-
+export default class MovieCard extends Component {
   constructor(dataMovie: Movie) {
     super();
-    this.attachShadow({ mode: "open" });
-    if (this.shadowRoot) {
-      this.shadowRoot.innerHTML = `
-      <link rel="stylesheet" href="/public/styles/movie.css">
-      <div class="front">
-        <img class="front__poster" src="${dataMovie.poster_path}" alt="${dataMovie.title}" loading="lazy"/>
-      </div>
-      <div class="back">
-        <h1 class="back__title">${dataMovie.title}</h1>
-        <h2 class="back__release">${dataMovie.release_date}</h2>
-        <p class="back__sinopsis">${dataMovie.overview}</p>
-      </div>
-      `;
-    }
+    this.shadow.innerHTML = `
+    <link rel="stylesheet" href="/public/styles/movie.css">
+    <div class="front">
+      <img class="front__poster" src="${dataMovie.poster_path}" alt="${dataMovie.title}" loading="lazy"/>
+    </div>
+    <div class="back">
+      <h1 class="back__title">${dataMovie.title}</h1>
+    </div>
+    `;
+  }
+
+  connectedCallback(): void {
+    const colors: string[] = [
+      "hsla(218, 18%, 43%, 1)",
+      "hsla(219, 18%, 32%, 1)",
+      "hsla(158, 51%, 74%, 1)",
+      "hsla(338, 96%, 53%, 1)",
+      "hsla(218, 77%, 38%, 1)",
+    ];
+    const back = this.shadow.querySelector(".back") as HTMLElement;
+    back.style.backgroundColor = colors[Utils.getRandomInt(0,5)];
   }
 }
 
---
+++ src/utils/Utils.ts
@@ -0,0 +1,5 @@
+export default class Utils {
+  static getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+}
