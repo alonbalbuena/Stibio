467e62f81fec2e5474750ab6a2380ffaa518e1df
alonbalbuena <alonbalbuena@gmail.com>
2021-03-20 02:42:29 +0100

    ✨ add watched button

---
+++ src/components/WatchedButton.ts
@@ -0,0 +1,34 @@
+import Component from "./Component.js";
+
+export default class WatchedButton extends Component {
+  constructor() {
+    super();
+    this.shadow.innerHTML = `
+    <link type="text/css" rel="stylesheet" href="./public/styles/watch.css" />
+    <div class="watch"><img class="watch__eye" src="/dist/public/icons/eye.svg" /></div>`;
+  }
+  connectedCallback(): void {
+    this.shadow.querySelector(".watch")?.addEventListener(
+      "click",
+      () => {
+        this.changeRoute();
+      },
+      false
+    );
+  }
+
+  changeRoute(): void {
+    this.toggleAnimation();
+    //this time has to fit with the transition
+    setTimeout(() => {
+      this.toggleAnimation();
+    }, 1000);
+  }
+
+  toggleAnimation(): void {
+    const watchButton = this.shadow.querySelector(".watch");
+    watchButton?.classList.toggle(`${watchButton.classList[0]}--transition`);
+  }
+}
+
+window.customElements.define("watched-button", WatchedButton);
