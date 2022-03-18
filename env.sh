705f25b4a175c77f849909b6d0d5435ede72279a
Alonso <alonbalbuena@gmail.com>
2022-03-18 21:53:41 +0100

    👷 change dev server

--- env.sh
+++ env.sh
@@ -4,7 +4,8 @@ if [ ! $1 ];
   then
     echo "use an environment value : development or production";
   else
-    echo  mode $1 ;
+    echo  mode $1 activated;
+    # replace "env.production" to "env.development"
     sed 's/envs\..*;/envs\.'"$1"';/g' ./src/env/actualEnvironment.ts >> temp;
     rm ./src/env/actualEnvironment.ts;
     mv temp ./src/env/actualEnvironment.ts;
